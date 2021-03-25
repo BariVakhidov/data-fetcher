7831673c5d93b3a3a072ec63dd5912901c8c490e
bary <bari.vaxidov@mail.ru>
2021-03-25 15:58:49 +0300

    fix inline styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-//import store from './redux/store';
+// import store from './redux/store';
 
 const AppContainer = () => (
   <React.StrictMode>
-    <BrowserRouter>      
-        <App />
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
